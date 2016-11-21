204202cedd42fe2c1ca9436cfe3a4d64ed6a56d8
i5ting <i5ting@126.com>
2016-11-21 21:16:21 +0800

    lazycommmit, please ignore

--- bin/cli.js
+++ bin/cli.js
@@ -6,8 +6,22 @@
 var argv = require('yargs')
 
 argv.usage('Usage: $0 <command> [options]')
-  .command('init', 'init db.js with cli')
-  .command('server', 'admin with http-server ui')
+  .command('init', 'init db.js with cli', function (yargs) {
+    return yargs.option('url', {
+      alias: 'u',
+      default: 'http://yargs.js.org/'
+    })
+  }, function (argv) {
+    console.log(argv.url)
+  })
+  .command('server', 'admin with http-server ui', function (yargs) {
+    return yargs.option('url', {
+      alias: 'u',
+      default: 'http://yargs.js.org/'
+    })
+  }, function (argv) {
+    console.log(argv.url)
+  })
   .example('$0 count -f foo.js', 'count the lines in the given file')
   .help('h')
   .alias('h', 'help')
