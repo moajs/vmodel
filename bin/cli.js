755b7a08c42d59cdbe642510f1649b29b2968252
i5ting <i5ting@126.com>
2016-11-21 21:11:04 +0800

    lazycommmit, please ignore

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
-var argv = process.argv
-argv.shift()
+// var filePath = __dirname
+// var currentPath = process.cwd()
 
-var filePath = __dirname
-var currentPath = process.cwd()
+var argv = require('yargs')
 
-console.log(filePath)
-console.log(currentPath)
+argv.usage('Usage: $0 <command> [options]')
+  .command('init', 'init db.js with cli')
+  .command('server', 'admin with http-server ui')
+  .example('$0 count -f foo.js', 'count the lines in the given file')
+  .help('h')
+  .alias('h', 'help')
+  .argv
